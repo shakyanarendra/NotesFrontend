e22cb36f27e005c413af79b7510f825af9d964f0
shakyanarendra <133335362+shakyanarendra@users.noreply.github.com>
2025-06-30 21:00:43 +0530

    Update Signup.jsx

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import axios from "axios";
 
 const Signup = () => {
+   const navigate = useNavigate();
   const [profilePreviewImage, setProfilePreviewImage] = useState("");
   const [profileImage, setProfileImage] = useState("");
   const [firstName, setfirstName] = useState("");
@@ -39,6 +40,7 @@ const Signup = () => {
       );
       console.log("Data: ", result);
       alert("User Entry Saved in Database");
+      navigate("/login");
     } catch (error) {
       console.log("Failed to Register User: ", error);
     } finally {
